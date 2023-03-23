797e250f4364662a49e3f6a8e4097ea9a04d7c8e
Ruth <elith.tati@gmail.com>
2023-03-23 16:24:03 -0300

    cuadros

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,67 +1,154 @@
 async function traerDatos() {
 
-    const data = await fetch("https://mindhub-xj03.onrender.com/api/amazing ").then(datos => datos.json())
-        .then(data => { return data })
-    return data
+  const data = await fetch("https://mindhub-xj03.onrender.com/api/amazing ").then(datos => datos.json())
+    .then(data => { return data })
+  return data
 }
+// Elementos de la tabla--------------------------------------------------------------------------------
+
+const tdDatosUnoA = document.getElementById('tabla_1a')
+const tdDatosUnoB = document.getElementById('tabla_1b')
+const tdDatosUnoC = document.getElementById('tabla_1c')
+
+function tablaUno(arrayWithAssistance) {
+
+  let assistanceDatos = arrayWithAssistance.filter(evento => evento.assistance)
+  let arrayNombres = []
+  let arrayPorcentajes = []
+  let arrayCapacidad = []
+  assistanceDatos.forEach(element => {
+    let eventoCapacidad = element.capacity
+    let eventoAsistencia = element.assistance
+    let porsentaje = eventoAsistencia * 100 / eventoCapacidad;
+    //console.log(porsentaje)
+    arrayPorcentajes.push(porsentaje)
+    arrayNombres.push(element.name)
+    arrayCapacidad.push(element.capacity)
+  })
 
 
-function eventoConMayorAsistencia(arrayassistance) {
+  let maxPorcentaje = Math.max(...arrayPorcentajes)
+  let minPorcentaje = Math.min(...arrayPorcentajes)
+  let maxCapacidad = Math.max(...arrayCapacidad)
 
-    let asistenciasTodas = arrayassistance.map(evento => evento.assistance)
-    let assistanceSet = new Set(asistenciasTodas)
-    let asistenciasArr = Array.from(assistanceSet)
-    console.log(asistenciasArr)
+  tdDatosUnoA.innerHTML = `${arrayNombres[arrayPorcentajes.indexOf(maxPorcentaje)]} (${maxPorcentaje}%)`
+  tdDatosUnoB.innerHTML = `${arrayNombres[arrayPorcentajes.indexOf(minPorcentaje)]} (${minPorcentaje}%)`
+  tdDatosUnoC.innerHTML = `${arrayNombres[arrayCapacidad.indexOf(maxCapacidad)]} (${maxCapacidad})`
 
-    asistenciasArr.forEach(elemento =>{
-        let mayorAsistencia= " "
-        let uno =  asistenciasArr[0]
-        if (elemento >= uno){
-            
-        }
+}
+//******************************************************************************** */
+const datosTablaDos = document.getElementById('tablaDos')
+const datosTablaTres = document.getElementById('tablaTres')
+
+function tipoDeTabla(arrcateg, fecha, tabla) {
+
+  let upEvent = []
+  let pastEvent = []
+  arrcateg.forEach(elemento => {
+    if (new Date(elemento.date) > fecha) {
+      upEvent.push(elemento)
+    } else {
+      pastEvent.push(elemento)
+    }
+  })
+  console.log(upEvent)
+  //console.log(pastEvent)
+  if (tabla == 'tablaDos') {
+
+    let categoriasSet = new Set(upEvent.map(evento => evento.category)); console.log(categoriasSet)
+
+    let columnasDeLaFila = ''
+    categoriasSet.forEach(element => {
+      let soloUnaCategoria = upEvent.filter(elemento => elemento.category == element)
+      console.log(soloUnaCategoria)
+
+      let sumCap = 0
+      let sumaPrice = 0
+      let estimate = 0
+
+      soloUnaCategoria.forEach(element => {
+        sumCap += element.capacity
+        sumaPrice += element.price
+        estimate += element.estimate
+      })
+      let porcEstimate = estimate * 100 / sumCap;
+
+      console.log('promEstimate es:', porcEstimate)
+      console.log('la suma es:', sumCap)
+      console.log('sumaPrice es:', sumaPrice)
+      console.log('estimate es:', estimate)
+
+      columnasDeLaFila += `
+    <tr>
+      <td>${element}</td>
+      <td>$${sumaPrice}</td>
+      <td>${porcEstimate}%</td>
+    </tr>`
+
+      datosTablaDos.innerHTML = columnasDeLaFila;
+
+      //console.log('promEstimate es:', porcEstimate)
     })
 
-   console.log(asistenciasArr[0])
+  }
+  if (tabla == 'tablaTres') {
+    let categoriasSet = new Set(pastEvent.map(evento => evento.category)); console.log(categoriasSet)
+
+    let columnasDeLaFila = ''
+    categoriasSet.forEach(element => {
+      let soloUnaCategoria = pastEvent.filter(elemento => elemento.category == element)
+      console.log(soloUnaCategoria)
+
+      let sumCap = 0
+      let sumaPrice = 0
+      let assistance = 0
+
+      soloUnaCategoria.forEach(element => {
+        sumCap += element.capacity
+        sumaPrice += element.price
+        assistance += element.assistance
+      })
+      let porcAssistance = assistance * 100 / sumCap;
+
+      console.log('promEstimate es:', porcAssistance)
+      console.log('la suma es:', sumCap)
+      console.log('sumaPrice es:', sumaPrice)
+
+
+      columnasDeLaFila += `
+    <tr>
+      <td>${element}</td>
+      <td>$${sumaPrice}</td>
+      <td>${porcAssistance}%</td>
+    </tr>`
+
+      datosTablaTres.innerHTML = columnasDeLaFila;
+
+      //console.log('promEstimate es:', porcEstimate)
+    })
+
+  }
 
 }
 
-async function iniciar() {
-    let data = await traerDatos()
-    console.log(data)
 
-    eventoConMayorAsistencia(data.events)
+
+//******************************************************************************** */
+
+
+async function iniciar() {
+  let data = await traerDatos()
+  console.log(data.events)
+  tablaUno(data.events)
+  var first = new Date(data.currentDate);
+  console.log(first)
+  let tablaDos = 'tablaDos'
+  tipoDeTabla(data.events, first, tablaDos)
+  let tablaTres = 'tablaTres'
+  tipoDeTabla(data.events, first, tablaTres)
 }
 iniciar()
 
-
-/* function tarjetasEventos(xxx) {
-    if (xxx.events.length == 0) {
-      divElementos.innerHTML = `<p>No hay coincidencias</p>`
-      return
-    }
-    let tarjetas = " "
-  
-    xxx.events.forEach(evento => {
-      var first = new Date(xxx.currentDate);
-      var second = new Date(evento.date);
-  
-      if (second > first) {
-        tarjetas += `<div class="col">
-  <div class="card h-100">
-  <img class="card-img-top" src="${evento.image}" alt="${evento.category}">
-  <div class="card-body">
-    <h5 class="card-title">${evento.name}</h5>
-    <p class="card-text">${evento.description}</p>
-  </div>
-  <div class="card-footer">
-    <small class="text-muted">$${evento.price}</small>
-    <a href="./details.html?id=${evento._id}" class="btn">Details</a>
-  </div>    
-  </div>
-  </div>`
-      }
-    })
-    divElementos.innerHTML = tarjetas;*/
 
 
 
